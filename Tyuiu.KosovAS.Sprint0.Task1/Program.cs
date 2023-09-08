f7614d029c42615ad8bf7d5f60e19c543c8e301c
Tyuiu.KosovAS@outlook.com <andrey.kosov.05@inbox.ru>
2023-09-08 22:05:28 +0500

    Task1

---
+++ Tyuiu.KosovAS.Sprint0.Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.KosovAS.Sprint0.Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Андрей";
+            int age = 17;
+            Console.WriteLine("Здравствуйте, меня зовут" + name + ", мне" + age + "лет");
+            Console.ReadKey();
+        }
+    }
+}
