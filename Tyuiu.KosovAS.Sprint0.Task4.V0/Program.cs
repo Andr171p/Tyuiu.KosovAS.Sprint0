b8af9e45e4339bfc85ef5e6da83651755ef2fefa
Tyuiu.KosovAS@outlook.com <andrey.kosov.05@inbox.ru>
2023-09-09 21:31:35 +0500

    Task4

---
+++ Tyuiu.KosovAS.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KosovAS.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.KosovAS.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(1, 5));
+            Console.WriteLine(DataService.Subtraction(10, 5));
+            Console.WriteLine(DataService.Multiplication(6, 6));
+            Console.WriteLine(DataService.Division(99, 9));
+            Console.ReadKey();
+        }
+    }
+}
